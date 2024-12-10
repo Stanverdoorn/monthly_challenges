aa7ab3318c460a00c6352d9db4092640fcef28f5
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2024-12-10 08:44:27 +0100

    Dynamic

--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("January")
-
-def february(request):
-    return HttpResponse("February")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "January challenge"
+    elif month == "february":
+        challenge_text = "February challenge"
+    elif month == "march":
+        challenge_text = "March challenge"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
