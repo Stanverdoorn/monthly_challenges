943462abf25b15c654a8d5afaec92feada85dd6c
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2024-12-11 13:41:06 +0100

    .

--- challenges/views.py
+++ challenges/views.py
@@ -4,6 +4,17 @@ from django.urls import reverse
 
 # Create your views here.
 
+def index(requests):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"   
+    return HttpResponse(response_data)
 
 monthly_challenges = {
     "january": "January challenge",
@@ -32,6 +43,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
